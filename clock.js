87515df3f47869d534f1faaec07a670ae35e36d7
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2023-11-07 14:30:49 +0200

    my new cv
    updating cv  - not yet complete

---
+++ clock.js
@@ -0,0 +1,35 @@
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const date = now.toDateString();
+
+    const clock = document.getElementById('clock');
+    const dateDisplay = document.getElementById('date');
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    dateDisplay.innerText = date;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
+
+
+// mouse hover
+
+document.body.addEventListener("mousemove", (event) => {
+    const element = document.createElement("div");
+    element.className = "animated-element";
+    document.body.appendChild(element);
+
+    element.style.left = event.clientX + "px";
+    element.style.top = event.clientY + "px";
+
+    // Remove the element after a short delay (adjust as needed)
+    setTimeout(() => {
+        element.remove();
+    }, 300);
+});
