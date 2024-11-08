440240afd862151817d4690eaedf558f1645f2bf
Lebohang Mofokeng <f8872022@fnb.co.za>
2024-11-08 16:52:33 +0200

    added clock on smaller screen

--- clock.js
+++ clock.js
@@ -9,8 +9,14 @@ function updateClock() {
     const clock = document.getElementById('clock');
     const dateDisplay = document.getElementById('date');
 
+    const clockB = document.getElementById('clockB');
+    const dateDisplayB = document.getElementById('dateB');
+
     clock.innerText = `${hours}:${minutes}`;
     dateDisplay.innerText = date;
+
+    clockB.innerText = `${hours}:${minutes}`;
+    dateDisplayB.innerText = date;
 }
 
 setInterval(updateClock, 1000);
@@ -44,40 +50,6 @@ document.querySelectorAll('.clickable-row').forEach(row => {
     });
   });
   
-// 3d effect for paging
-document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-  const pageContent = document.querySelector('.page-content');
-
-  let currentPageIndex = -1;
-
-  // Find the index of the current page in the navbar links
-  const currentPagePath = window.location.pathname;
-
-  navLinks.forEach((link, index) => {
-    const linkPath = link.getAttribute('href');
-    
-    if (linkPath === currentPagePath) {
-      currentPageIndex = index;
-    }
-
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const href = link.getAttribute('href');
-      const nextPageIndex = Array.from(navLinks).findIndex(item => item.getAttribute('href') === href);
- 
-      const direction = nextPageIndex > currentPageIndex ? 'forward' : 'backward';
-   
-      pageContent.classList.add(`page-flip-${direction}`);
-
-      currentPageIndex = nextPageIndex;
-    
-      setTimeout(() => {
-        window.location.href = href;
-      }, 500); 
-    });
-  });
-});
 
 
 
